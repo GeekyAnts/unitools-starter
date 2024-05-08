895afd092f58618b5e3643c5032fa729c1fc2048
ankit-tailor <tailorankit3345@gmail.com>
2024-05-08 12:21:09 +0530

    feat: expo router

---
+++ apps/expo/app/(tabs)/users.tsx
@@ -0,0 +1,3 @@
+import Users from 'screens/users'
+
+export default Users
---
+++ apps/expo/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@unitools/link': '@unitools/link-expo',
+            '@unitools/router': '@unitools/router-expo',
+            '@unitools/image': '@unitools/image-expo',
+          },
+        },
+      ],
+    ],
+  }
+}
